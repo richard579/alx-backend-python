d4b1d7b39a8458ef8623c66ca73c8717cb3f9610
richard579 <richardbeng358@outlook.com>
2024-01-15 15:47:03 +0000

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """spawns wait_random n times with the specified max_delay
     and returns the list of all the delays (float values)."""
-    futures = [wait_random(max_delay) for _ in range(n)]
+    futures = [task_wait_random(max_delay) for _ in range(n)]
     futures = asyncio.as_completed(futures)
     delays = [await future for future in futures]
     return delays
